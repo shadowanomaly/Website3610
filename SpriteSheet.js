f9a710ab97cb4d325bef722896478c966c71592d
shadowanomaly <56459683+shadowanomaly@users.noreply.github.com>
2020-02-25 15:05:57 -0500

    Add files via upload

---
+++ SpriteSheet.js
@@ -0,0 +1,208 @@
+parrot={
+	src : "spritesheet2.png",
+	img: undefined,
+	frame:0,
+	offset: 0,
+	"frames": [
+		{
+			"filename": "0.png",
+			"frame": {
+				"x": 1,
+				"y": 1,
+				"w": 287,
+				"h": 264
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 287,
+				"h": 264
+			},
+			"sourceSize": {
+				"w": 287,
+				"h": 264
+			}
+		},
+		{
+			"filename": "1.png",
+			"frame": {
+				"x": 290,
+				"y": 1,
+				"w": 287,
+				"h": 264
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 287,
+				"h": 264
+			},
+			"sourceSize": {
+				"w": 287,
+				"h": 264
+			}
+		},
+		{
+			"filename": "2.png",
+			"frame": {
+				"x": 1,
+				"y": 267,
+				"w": 286,
+				"h": 264
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 286,
+				"h": 264
+			},
+			"sourceSize": {
+				"w": 286,
+				"h": 264
+			}
+		},
+		{
+			"filename": "3.png",
+			"frame": {
+				"x": 289,
+				"y": 267,
+				"w": 287,
+				"h": 264
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 287,
+				"h": 264
+			},
+			"sourceSize": {
+				"w": 287,
+				"h": 264
+			}
+		},
+		{
+			"filename": "4.png",
+			"frame": {
+				"x": 1,
+				"y": 533,
+				"w": 287,
+				"h": 264
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 287,
+				"h": 264
+			},
+			"sourceSize": {
+				"w": 287,
+				"h": 264
+			}
+		},
+		{
+			"filename": "5.png",
+			"frame": {
+				"x": 290,
+				"y": 533,
+				"w": 286,
+				"h": 264
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 286,
+				"h": 264
+			},
+			"sourceSize": {
+				"w": 286,
+				"h": 264
+			}
+		},
+		{
+			"filename": "6.png",
+			"frame": {
+				"x": 579,
+				"y": 1,
+				"w": 287,
+				"h": 264
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 287,
+				"h": 264
+			},
+			"sourceSize": {
+				"w": 287,
+				"h": 264
+			}
+		},
+		{
+			"filename": "7.png",
+			"frame": {
+				"x": 578,
+				"y": 267,
+				"w": 287,
+				"h": 264
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 287,
+				"h": 264
+			},
+			"sourceSize": {
+				"w": 287,
+				"h": 264
+			}
+		},
+		{
+			"filename": "8.png",
+			"frame": {
+				"x": 578,
+				"y": 533,
+				"w": 286,
+				"h": 264
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 286,
+				"h": 264
+			},
+			"sourceSize": {
+				"w": 286,
+				"h": 264
+			}
+		}
+	],
+	"meta": {
+		"app": "http://www.codeandweb.com/texturepacker",
+		"version": "1.0",
+		"image": "spritesheet2.png",
+		"format": "RGBA8888",
+		"size": {
+			"w": 867,
+			"h": 798
+		},
+		"scale": "1"
+	}
+}
