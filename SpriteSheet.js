2cbc343d6745836ee9ae092b40c8c8690f89cfa8
shadowanomaly <56459683+shadowanomaly@users.noreply.github.com>
2020-02-28 15:52:21 -0500

    Update SpriteSheet.js

--- SpriteSheet.js
+++ SpriteSheet.js
@@ -1,208 +1,52 @@
-parrot={
-	src : "spritesheet2.png",
-	img: undefined,
-	frame:0,
-	offset: 0,
-	"frames": [
-		{
-			"filename": "0.png",
-			"frame": {
-				"x": 1,
-				"y": 1,
-				"w": 287,
-				"h": 264
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 287,
-				"h": 264
-			},
-			"sourceSize": {
-				"w": 287,
-				"h": 264
-			}
-		},
-		{
-			"filename": "1.png",
-			"frame": {
-				"x": 290,
-				"y": 1,
-				"w": 287,
-				"h": 264
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 287,
-				"h": 264
-			},
-			"sourceSize": {
-				"w": 287,
-				"h": 264
-			}
-		},
-		{
-			"filename": "2.png",
-			"frame": {
-				"x": 1,
-				"y": 267,
-				"w": 286,
-				"h": 264
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 286,
-				"h": 264
-			},
-			"sourceSize": {
-				"w": 286,
-				"h": 264
-			}
-		},
-		{
-			"filename": "3.png",
-			"frame": {
-				"x": 289,
-				"y": 267,
-				"w": 287,
-				"h": 264
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 287,
-				"h": 264
-			},
-			"sourceSize": {
-				"w": 287,
-				"h": 264
-			}
-		},
-		{
-			"filename": "4.png",
-			"frame": {
-				"x": 1,
-				"y": 533,
-				"w": 287,
-				"h": 264
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 287,
-				"h": 264
-			},
-			"sourceSize": {
-				"w": 287,
-				"h": 264
-			}
-		},
-		{
-			"filename": "5.png",
-			"frame": {
-				"x": 290,
-				"y": 533,
-				"w": 286,
-				"h": 264
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 286,
-				"h": 264
-			},
-			"sourceSize": {
-				"w": 286,
-				"h": 264
-			}
-		},
-		{
-			"filename": "6.png",
-			"frame": {
-				"x": 579,
-				"y": 1,
-				"w": 287,
-				"h": 264
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 287,
-				"h": 264
-			},
-			"sourceSize": {
-				"w": 287,
-				"h": 264
-			}
-		},
-		{
-			"filename": "7.png",
-			"frame": {
-				"x": 578,
-				"y": 267,
-				"w": 287,
-				"h": 264
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 287,
-				"h": 264
-			},
-			"sourceSize": {
-				"w": 287,
-				"h": 264
-			}
-		},
-		{
-			"filename": "8.png",
-			"frame": {
-				"x": 578,
-				"y": 533,
-				"w": 286,
-				"h": 264
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 286,
-				"h": 264
-			},
-			"sourceSize": {
-				"w": 286,
-				"h": 264
-			}
-		}
-	],
-	"meta": {
-		"app": "http://www.codeandweb.com/texturepacker",
-		"version": "1.0",
-		"image": "spritesheet2.png",
-		"format": "RGBA8888",
-		"size": {
-			"w": 867,
-			"h": 798
-		},
-		"scale": "1"
-	}
+let img = new Image();
+img.src = 'spritesheet2.png';
+img.onload = function() {
+  
+};
+
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+const scale = 2;
+const width = 57;
+const height = 91;
+const scaledWidth = scale * width;
+const scaledHeight = scale * height;
+
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+  ctx.drawImage(img,
+                frameX * width, frameY * height, width, height,
+                canvasX, canvasY, scaledWidth, scaledHeight);
+}
+
+function init() {
+  window.requestAnimationFrame(step);
+}
+
+const cycleLoop = [0,1,2];
+let currentLoopIndex = 0;
+let frameCount = 0;
+let currentDirection = 0;
+
+let animationFrameId;
+
+function step() {
+  frameCount++;
+  if (frameCount < 50) {
+    window.requestAnimationFrame(step);
+    return;
+  }
+  frameCount = 0;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawFrame(cycleLoop[currentLoopIndex], currentDirection, 0, 0);
+  currentLoopIndex++;
+  if (currentLoopIndex >= cycleLoop.length) {
+    currentLoopIndex = 0;
+    currentDirection++; // Next row/direction in the sprite sheet
+  }
+  // Reset to the "down" direction once we've run through them all
+  if (currentDirection >= 3) {
+    currentDirection = 0;
+  }
+  window.requestAnimationFrame(step);
 }
