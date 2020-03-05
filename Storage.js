d114ebbea6a9afa7fde6c8dff077eae7fb080c46
shadowanomaly <56459683+shadowanomaly@users.noreply.github.com>
2020-03-05 11:55:22 -0500

    Update Storage.js

--- Storage.js
+++ Storage.js
@@ -1,4 +1,4 @@
-function myFunction3(){
+
 // Check browser support
 if (typeof(Storage) !== "undefined") {
   // Store
@@ -8,19 +8,7 @@ if (typeof(Storage) !== "undefined") {
 } else {
   document.getElementById("textarea2").innerHTML = "Sorry, your browser does not support Web Storage...";
 }
-}
 
-function myFunction4(){
-// Check browser support
-if (typeof(Storage) !== "undefined") {
-  // Store
-  localStorage.setItem("title"," ");
-  // Retrieve
-  document.getElementById("textarea3").innerHTML = localStorage.getItem("title");
-} else {
-  document.getElementById("textarea3").innerHTML = "Sorry, your browser does not support Web Storage...";
-}
-}
 
 
 function myFunction() {
