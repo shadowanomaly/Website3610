e1b64aea751f7e08c2061183cb6a44e655f3c66d
shadowanomaly <56459683+shadowanomaly@users.noreply.github.com>
2020-03-09 17:50:27 -0400

    Update Storage.js

--- Storage.js
+++ Storage.js
@@ -1,34 +1,15 @@
+  localStorage.setItem("textarea1");
+  localStorage.setItem("textarea2");
+console.log(localStorage.getItem("textarea1"));
+console.log(localStorage.getItem("textarea2"));
 
-/* Check browser support
-if (typeof(Storage) !== "undefined") {
-  // Store
-  localStorage.setItem("textarea1"," ");
-  // Retrieve
-  document.getElementById("textarea2").innerHTML = localStorage.getItem("textarea1");
-} else {
-  document.getElementById("textarea2").innerHTML = "Sorry, your browser does not support Web Storage...";
-}
-*/
-
-function save(){
-var text_to_save=document.getElementById('textarea1').value;
-localStorage.setItem("text", text_to_save); // save the item
-}
-
-function retrieve(){
-var text=localStorage.getItem("text"); // retrieve
-document.getElementById('textarea2').innerHTML = text; // display
-}
-
+//For notes
 function myFunction() {
   var x = document.getElementById("textarea1").value;
   document.getElementById("textarea2").innerHTML = "You Wrote: " + x;
 }
-
-
+//For title
 function myFunction2() {
   var x = document.getElementById("title").value;
   document.getElementById("textarea3").innerHTML = "Title: " + x;
 }
-
-
