75bde7dc913f493e27e82412b9a04df638ba1f12
shadowanomaly <56459683+shadowanomaly@users.noreply.github.com>
2020-03-09 18:17:21 -0400

    Update Storage.js

--- Storage.js
+++ Storage.js
@@ -1,13 +1,23 @@
+//TextArea Storage
 function save(){
 var text_to_save=document.getElementById("textarea1"," ").value;
 localStorage.setItem("text", text_to_save); // save the item
 }
-
 function retrieve(){
 var text=localStorage.getItem("text"); // retrieve
 document.getElementById('textarea2').innerHTML = text; // display
 }
- 
+//============================================================================================================
+//Title Storage
+function save2(){
+var text2_to_save=document.getElementById("title"," ").value;
+localStorage.setItem("text2", text2_to_save); // save the item
+}
+function retrieve2(){
+var text2=localStorage.getItem("text2"); // retrieve
+document.getElementById('textarea3').innerHTML = text2; // display
+}
+//===========================================================================================================
 //For notes
 function myFunction() {
   var x = document.getElementById("textarea1").value;
