714e08ce9f7e972fd85cf9a76749c7ed3f143d7b
Elsie Urena <56459683+shadowanomaly@users.noreply.github.com>
2020-03-19 15:09:48 -0400

    Update SpriteSheet.js

--- SpriteSheet.js
+++ SpriteSheet.js
@@ -1,5 +1,8 @@
 let img = new Image();
 img.src = 'spritesheet2.png';
+img.onload = function() {
+  
+};
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 const scale = 2;
@@ -7,19 +10,20 @@ const width = 57;
 const height = 91;
 const scaledWidth = scale * width;
 const scaledHeight = scale * height;
-
 function drawFrame(frameX, frameY, canvasX, canvasY) {
   ctx.drawImage(img,
                 frameX * width, frameY * height, width, height,
                 canvasX, canvasY, scaledWidth, scaledHeight);
 }
-
-const cycleLoop = [1,2,3,4,5,6,7,8,9,0,9];
+function init() {
+  window.requestAnimationFrame(step);
+}
+const cycleLoop = [0,1,2,3,4,5,6,7,8,9,0,9];
 let currentLoopIndex = 0;
 let frameCount = 10;
-
+let currentDirection = 0;
+let animationFrameId;
 function step() {
-  document.getElementById("image");
   frameCount++;
   if (frameCount < 50) {
     window.requestAnimationFrame(step);
@@ -29,10 +33,13 @@ function step() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawFrame(cycleLoop[currentLoopIndex], currentDirection, 0, 0);
   currentLoopIndex++;
-  if(cycleLoop[currentLoopIndex] == 11){
-  drawFrame(1, 0, 500, 0);
-  drawFrame(0, 0, 700, 0);
+  if (currentLoopIndex >= cycleLoop.length) {
+    currentLoopIndex = 0;
+    currentDirection++; // Next row/direction in the sprite sheet
   }
-  currentLoopIndex++;
+  // Reset to the "down" direction once we've run through them all
+  if (currentDirection >= 1) {
+    currentDirection = 0;
+  }
   window.requestAnimationFrame(step);
 }
