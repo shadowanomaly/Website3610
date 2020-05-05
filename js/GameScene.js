fadc654c1c37003be000af8955ef5d51ea6f386c
Elsie Urena <56459683+shadowanomaly@users.noreply.github.com>
2020-05-05 18:58:45 -0400

    Update GameScene.js

--- js/GameScene.js
+++ js/GameScene.js
@@ -7,22 +7,7 @@
 	init() {
 		
 	};
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-        physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 300 },
-            debug: false
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+
 };
 var player;
 var coin;
