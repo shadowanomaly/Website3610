2d95b3024199fec598f6623aeb5a2c6618a7a70b
Elsie Urena <56459683+shadowanomaly@users.noreply.github.com>
2020-05-08 15:50:33 -0400

    Create Preloader.js

---
+++ js/Preloader.js
@@ -0,0 +1,36 @@
+Game.Preloader = function (game){
+
+    this.preloaderBar = null;
+    
+};
+
+Game.Preloader.prototype = {
+      preload:function(){
+      
+          this.preloadBar = this.addsprite(this.world.centerX,
+                                          this.world.centerY,'preloaderBar');
+                                          
+          this.preloadBar.anchor.setTo(0.5,0,0.5);
+          
+          this.time.advancedTiming = true;
+          
+          this.load.setPreloadSpirite(this.preloadBar);
+          
+          //LOAD ALL ASSETS
+          
+          
+          },
+          
+          create:function(){
+          
+              this.state.start('Level1');
+          
+          }
+          
+          
+          
+          
+          
+          }
+          
+    
